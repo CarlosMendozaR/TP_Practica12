aecc25c8b0dce776d3adfaef35d7622201e8af45
Carlos <halomau@hotmail.com>
2018-10-08 22:22:53 -0500

    todos los autos faltan agregarse a la lista

--- P12/Listas/Form1.cs
+++ P12/Listas/Form1.cs
@@ -14,8 +14,8 @@ namespace Listas
         Auto deportivo2 = new Auto("MITSUBISHI Lancer Evo IX", "2017", 15000, "S", 779000);
         Auto atv1 = new Auto("LAN ROVER Discovery Sport", "2018", 1000, "A", 1256000);
         Auto atv2 = new Auto("BMW X6","2017",0,"S",1576000);
-        Auto superlujo1;
-        Auto superlujo2;
+        Auto superlujo1 = new Auto("MERCEDES BENZ Clase S", "2016", 0, "A", 2412000);
+        Auto superlujo2 = new Auto("MCLAREN 720S","2019",0,"S",5552000);
 
         public Form1()
         {
