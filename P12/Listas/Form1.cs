c5dea2344e622144ae8c9d72fb13c88f09408720
Gerardo Chapa <gerardochapa1000@hotmail.com>
2018-10-08 23:15:12 -0500

    sadff

--- P12/Listas/Form1.cs
+++ P12/Listas/Form1.cs
@@ -14,11 +14,11 @@ namespace Listas
         Auto deportivo1 = new Auto("TOYOTA Supra", "2014", 96000, "S", 849000);
         Auto deportivo2 = new Auto("MITSUBISHI Lancer Evo IX", "2017", 15000, "S", 779000);
         Auto atv1 = new Auto("LAN ROVER Discovery Sport", "2018", 1000, "A", 1256000);
-        Auto atv2 = new Auto("BMW X6", "2017", 0, "S", 1576000);
-        Auto superlujo1 = new Auto("LAMBROGHINI Veneno", "2018", 0, "S", 16000000);
-        Auto superlujo2 = new Auto("FERRARI F12", "2018", 0, "S", 3798660);
+        Auto atv2 = new Auto("BMW X6","2017",0,"S",1576000);
+        Auto superlujo1 = new Auto("MERCEDES BENZ Clase S", "2016", 0, "A", 2412000);
+        Auto superlujo2 = new Auto("MCLAREN 720S","2019",0,"S",5552000);
 
-
+       
         public Form1()
         {
 
