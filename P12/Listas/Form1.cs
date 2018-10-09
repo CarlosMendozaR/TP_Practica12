1370a51668901d13635ecb3755c7207c55afca5e
Gerardo Chapa <gerardochapa1000@hotmail.com>
2018-10-08 23:12:02 -0500

    asdadssa

--- P12/Listas/Form1.cs
+++ P12/Listas/Form1.cs
@@ -10,14 +10,18 @@ namespace Listas
     {
         List<Auto> miLista = new List<Auto>();
 
+
+        Auto deportivo1 = new Auto("TOYOTA Supra", "2014", 96000, "S", 849000);
+        Auto deportivo2 = new Auto("MITSUBISHI Lancer Evo IX", "2017", 15000, "S", 779000);
+        Auto atv1 = new Auto("LAN ROVER Discovery Sport", "2018", 1000, "A", 1256000);
+        Auto atv2 = new Auto("BMW X6", "2017", 0, "S", 1576000);
+        Auto superlujo1 = new Auto("LAMBROGHINI Veneno", "2018", 0, "S", 16000000);
+        Auto superlujo2 = new Auto("FERRARI F12", "2018", 0, "S", 3798660);
+
+
         public Form1()
         {
-            Auto deportivo1 = new Auto("TOYOTA Supra", "2014", 96000, "S", 849000);
-            Auto deportivo2 = new Auto("MITSUBISHI Lancer Evo IX", "2017", 15000, "S", 779000);
-            Auto atv1 = new Auto("LAN ROVER Discovery Sport", "2018", 1000, "A", 1256000);
-            Auto atv2 = new Auto("BMW X6", "2017", 0, "S", 1576000);
-            Auto superlujo1 = new Auto("LAMBROGHINI Veneno", "2018", 0, "S", 16000000);
-            Auto superlujo2 = new Auto("FERRARI F12", "2018", 0, "S", 3798660);
+
 
             miLista.Add(deportivo1);
             miLista.Add(deportivo2);
